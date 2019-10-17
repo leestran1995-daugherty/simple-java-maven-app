b293b4dc38df01465d3ab656cff7794764bdfdb8
Lee Tran <leetran@MSP42LST061022.local>
2019-10-17 14:35:10 -0500

    add stupid

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,8 @@ public class App
 
     private final String message = "Hello World!";
 
+    private final String stupid = "stupid";
+
     public App() {}
 
     public static void main(String[] args) {
